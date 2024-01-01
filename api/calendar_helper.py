5c627e2852410c729e6b41adf2a298cdf13c2bda
Tuấn Anh <phantuananh0202@gmail.com>
2024-01-02 03:01:40 +0700

    add check for some important fields
    For fields "name", "room". It needs to be 'required' keys to avoid any errors that can happen when importing to the calendar. And with "code", "class" and "lecturer" fields just need to be necessary fields.

--- api/calendar_helper.py
+++ api/calendar_helper.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from math import floor
 from typing import List, Dict
+from exceptions import ScheduleException
 
 
 class CalendarHelper:
@@ -24,9 +25,21 @@ class CalendarHelper:
             List[Dict[str, str]]: The formatted list of subjects data with added fields such as
             'repeat', 'start_period_date', 'end_period_date', and 'color'.
         """
+        required_keys = ["name", "room"]
+        necessary_keys = ["code", "class", "lecturer"]
+
         subject_colors: Dict[str, str] = {}
 
         for subject in subjects_data:
+            for key in required_keys:
+                if key not in subject.keys():
+                    raise ScheduleException(f"Missing '{key}' field.")
+
+            for key in necessary_keys:
+                if key not in subject.keys():
+                    print(f"Warning: Missing '{key}' field of '{subject['name']}'. This field will be blank.")
+                    subject[key] = ""
+
             from_date = datetime.fromisoformat(subject["from_date"])
             to_date = datetime.fromisoformat(subject["to_date"])
 
